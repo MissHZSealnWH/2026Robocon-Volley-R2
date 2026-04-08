f2886da336882606be8ebc08e7861f91baaec81c
CLLR <3228847917@qq.com>
2026-04-09 02:51:51 +0800

    增加回球任务

--- Mytask/Task_Init.c
+++ Mytask/Task_Init.c
@@ -10,31 +10,42 @@ extern uint8_t usart5_buff[30];
 	
 void Task_Init(){
 
-	  RS485Init(&rs485bus, &huart6, GPIOA, GPIO_PIN_4);// 初始化485总线管理器
+	RS485Init(&rs485bus, &huart6, GPIOA, GPIO_PIN_4);// 初始化485总线管理器
 	
 	 //遥控器
    __HAL_UART_ENABLE_IT(&huart5, UART_IT_IDLE);
    HAL_UARTEx_ReceiveToIdle_DMA(&huart5, usart5_buff, sizeof(usart5_buff));
    __HAL_DMA_DISABLE_IT(huart5.hdmarx, DMA_IT_HT);
 
-
-
-
 	vPortEnterCritical();
 	
-//	xTaskCreate(Remote,
-//         "Remote",
-//          400,
-//          NULL,
-//          3,
-//          &Remote_Handle); 
+	xTaskCreate(Remote,
+      	"Remote",
+        400,
+        NULL,
+        3,
+        &Remote_Handle); 
 					
 	xTaskCreate(Hit_Task,
 			 "Hit_Task",
-				400,
+				256,
 				NULL,
-				4,
+				3,
 				&Hit_Task_Handle); 
+				
+	xTaskCreate(Control_Remote,
+			 "Control_Remote",
+				256,
+				NULL,
+				3,
+				&Control_Remote_Handle); 
+	
+	xTaskCreate(Ball_back,
+			 "Ball_back",
+				256,
+				NULL,
+				3,
+				&Ball_back_Handle); 
 					
 	vPortExitCritical();
 	
