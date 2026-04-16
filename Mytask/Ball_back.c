a0d7858d5d261e0978610fe9914d1770ddb5d8b0
CLLR <3228847917@qq.com>
2026-04-16 16:29:56 +0800

    换为F407

--- Mytask/Ball_back.c
+++ Mytask/Ball_back.c
@@ -3,23 +3,43 @@
 #include "Task_Init.h"
 #include "step.h"
 #include "motorEx.h"
+#include "task.h"
 
 extern RM3508_TypeDef Rm3508;
 
-//RobStride_Expect R_left_expect = {
-//	.expect_angle = 0.0f,
-//	.expect_omega = 0.0f,
-//	.expect_torque = 0.0f,
-//	.kd = 0.0f,
-//	.kp = 0.0f
-//};
-//RobStride_Expect R_right_expect = {
-//	.expect_angle = 0.0f,
-//	.expect_omega = 0.0f,
-//	.expect_torque = 0.0f,
-//	.kd = 0.0f,
-//	.kp = 0.0f
-//};
+RobStride_Expect R_left_expect = {
+	.expect_angle = -0.355f,
+	.expect_omega = 0.0f,
+	.expect_torque = -3.3f,
+	.kp = 280.0f,
+	.kd = 8.0f
+
+};
+RobStride_Expect R_right_expect = {
+	.expect_angle = 0.39f,
+	.expect_omega = 0.0f,
+	.expect_torque = 3.3f,
+	.kp = 280.0f,
+	.kd = 8.0f
+};
+
+RobStride_Reset R_left_reset = {
+	.reset_angle = 0.0f,
+	.reset_omega = 0.0f,
+	.reset_torque = 0.5f,
+	.kp = 10.0f,
+	.kd = 1.0f
+};
+RobStride_Reset R_right_reset = {
+	.reset_angle = 0.0f,
+	.reset_omega = 0.0f,
+	.reset_torque = -0.5f,
+	.kp = 10.0f,
+	.kd = 1.0f
+};
+
+RobStride_t R_left;
+RobStride_t R_right;
 
 CubicParam_t traj_left;
 CubicParam_t traj_right;
@@ -27,21 +47,14 @@ CubicParam_t traj_right;
 TrajectoryState_t traj_left_state;
 TrajectoryState_t traj_right_state;
 
-RobStride_Expect R_left_expect;
-RobStride_Expect R_right_expect;
+//RobStride_Expect R_left_expect;
+//RobStride_Expect R_right_expect;
 
-RobStride_Reset R_left_reset;
-RobStride_Reset R_right_reset;
-
-RobStride_t R_left;
-RobStride_t R_right;
-
-uint8_t ball_back_trigger = 0;
-uint8_t flag = 0;
-static uint8_t traj_started = 0;
-uint8_t the_symbol = 0;
+uint8_t flag = 0;// 复位标志
+static uint8_t ball_back_trigger = 0;// 击球标志
+static uint8_t traj_started = 0;// 轨迹规划开始标志
 static GPIO_PinState key1, key2, key3, key4;
-float time = 0.0f;
+float time = 0.25f;// 轨迹规划时间
 
 TaskHandle_t Ball_back_Handle;
 void Ball_back(void *pvParameters)
@@ -72,11 +85,9 @@ void Ball_back(void *pvParameters)
 		key4 = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_3);
 		if(key1 == GPIO_PIN_SET || key2 == GPIO_PIN_SET || key3 == GPIO_PIN_SET || key4 == GPIO_PIN_SET)
 			{
-				the_symbol = 1;
 				ball_back_trigger = 1;
 			}
-	if(the_symbol == 1)
-	{
+
 		if (ball_back_trigger == 1 && traj_started == 0)   // 一次触发
 		{
 				Cubic_SetTrajectory(
@@ -103,10 +114,11 @@ void Ball_back(void *pvParameters)
 
 		if(ball_back_trigger == 1)
 		{
-			
+		 if (traj_left.is_running || traj_right.is_running)
+		 {
 			Cubic_GetFullState(&traj_left,  xTaskGetTickCount(), &traj_left_state);
 			Cubic_GetFullState(&traj_right, xTaskGetTickCount(), &traj_right_state);
-			
+				
 			RobStrideMotionControl(&R_left, 0x01, 
 			R_left_expect.expect_torque, 
 			traj_left_state.pos,
@@ -120,34 +132,40 @@ void Ball_back(void *pvParameters)
 			traj_right_state.vel,
 			R_right_expect.kp,
 			R_right_expect.kd);
-	}
-		vTaskDelay(300);
-	
-	 		RobStrideMotionControl(&R_left, 0x01, 
-			R_left_reset.reset_torque, 
-			R_left_reset.reset_angle,
-			R_left_reset.reset_omega,
-			R_left_reset.kp,
-			R_left_reset.kd);
+		 }
+			else
+			{
+			RobStrideMotionControl(&R_left, 0x01,
+			0.0f, traj_left.target_pos, 0.0f,
+			R_left_expect.kp, R_left_expect.kd);
 
-	 		RobStrideMotionControl(&R_left, 0x01, 
-			R_right_reset.reset_torque, 
-			R_right_reset.reset_angle,
-			R_right_reset.reset_omega,
-			R_right_reset.kp,
-			R_right_reset.kd);
-	
-	   traj_started = 0;
-	
-	    vTaskDelay(200);
-		
-// 此处仅测试使用	
-//		if(flag == 0 && ball_back_trigger == 0)
-//			{				
-//			RobStrideMotionControl(&R_left, 0x01, 0, R_left.state.rad, 0, 0, 0);
-//			RobStrideMotionControl(&R_right, 0x02, 0, R_right.state.rad, 0, 0, 0);
-//			}
+			RobStrideMotionControl(&R_right, 0x02,
+			0.0f, traj_right.target_pos, 0.0f,
+			R_right_expect.kp, R_right_expect.kd);
+				
+			traj_started = 0;
+			ball_back_trigger = 0;
+			}
 		}
+
+		if( flag == 0 && ball_back_trigger == 0)
+			{				
+			RobStrideMotionControl(&R_left, 0x01, 
+				R_left_reset.reset_torque, 
+				R_left_reset.reset_angle, 
+				R_left_reset.reset_omega, 
+				R_left_reset.kp, 
+				R_left_reset.kd);
+			RobStrideMotionControl(&R_right, 0x02, 
+				R_right_reset.reset_torque, 
+				R_right_reset.reset_angle, 
+				R_right_reset.reset_omega, 
+				R_right_reset.kp, 
+				R_right_reset.kd);
+//			RobStrideMotionControl(&R_left, 0x01, 0, R_left_reset.reset_angle, 0, 0, 0);
+//			RobStrideMotionControl(&R_right, 0x02, 0, R_right_reset.reset_angle, 0, 0, 0);
+			}
+			
 	 vTaskDelayUntil(&last_wake, pdMS_TO_TICKS(2));
 	}
 }
@@ -159,14 +177,3 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
 	RobStrideRecv_Handle(&R_left, &hcan1, ID, buf);
   RobStrideRecv_Handle(&R_right, &hcan1, ID, buf);
 }
-
-//void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
-//{
-//	uint8_t buf[8];
-//	uint32_t ID = CAN_Receive_DataFrame(&hcan1, buf);
-//	RobStrideRecv_Handle(&R_left, &hcan1, ID, buf);
-//  RobStrideRecv_Handle(&R_right, &hcan1, ID, buf);
-//	
-//	uint8_t buff[8];
-//	RM3508_Receive(&Rm3508, buff);
-//}
